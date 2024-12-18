c72f1a4804d6bdc88dc64c6dbf68076c2ba9b783
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-12-18 08:12:46 -0400

    fix:ci/cd

--- main.tf
+++ main.tf
@@ -2,7 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Grupo de seguridad para la instancia
+variable "ssh_public_key" {
+  description = "Clave pública SSH para acceder al EC2"
+}
+
 resource "aws_security_group" "isc_system_core_sg" {
   name        = "isc-system-core-security-group"
   description = "Security group con puerto 8000 abierto"
@@ -30,15 +33,15 @@ resource "aws_security_group" "isc_system_core_sg" {
   }
 }
 
-# Llave SSH para acceso a la instancia
+# Llave SSH para acceder al EC2
 resource "aws_key_pair" "backend-ssh" {
   key_name   = "isc-system-backend-ssh"
-  public_key = file("isc-system-backend-ssh.key.pub")
+  public_key = var.ssh_public_key
 }
 
 # Instancia EC2 para el backend
 resource "aws_instance" "isc_system_core_server" {
-  ami           = "ami-0e2c8caa4b6378d8c" 
+  ami           = "ami-0e2c8caa4b6378d8c"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.backend-ssh.key_name
   vpc_security_group_ids = [aws_security_group.isc_system_core_sg.id]
@@ -52,13 +55,13 @@ resource "aws_instance" "isc_system_core_server" {
       type        = "ssh"
       host        = self.public_ip
       user        = "ec2-user"
-      private_key = file("isc-system-core-backend-ssh.key")
+      private_key = file("isc-system-backend-ssh") }
     }
 
     inline = [
       "sudo apt update -y",
       "sudo apt upgrade -y",
-      "sudo apt install -y nodejs npm git",  #
+      "sudo apt install -y nodejs npm git", 
       "mkdir -p /home/ec2-user/backend",
       "cd /home/ec2-user",
       "git clone https://github.com/Khenya/ISC-System-UPB-Core.git backend", 
@@ -68,14 +71,15 @@ resource "aws_instance" "isc_system_core_server" {
       "echo 'cd /home/ec2-user/backend' >> /home/ec2-user/backend/start.sh",
       "echo 'nohup node server.js > nohup.out 2>&1 &' >> /home/ec2-user/backend/start.sh", 
       "chmod +x /home/ec2-user/backend/start.sh",
-      "bash /home/ec2-user/backend/start.sh"
+      "bash /home/ec2-user/backend/start.sh" 
     ]
   }
 }
 
-# Elastic IP para la instancia
+# Elastic IP para la instancia EC2
 resource "aws_eip" "backend_eip" {
   instance = aws_instance.isc_system_core_server.id
+
   tags = {
     Name = "Backend-ISC-UPB-System-App-EIP"
   }
