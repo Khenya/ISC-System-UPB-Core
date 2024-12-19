76f01340344d97552b8cfcc2312d34ae138b9aee
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-12-18 23:48:09 -0400

    terraform

--- main.tf
+++ main.tf
@@ -2,19 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "ssh_public_key" {
-  description = "Clave pública SSH para acceder al EC2"
-}
-
-resource "aws_security_group" "isc_system_core_sg" {
-  name        = "isc-system-core-security-group"
-  description = "Security group con puerto 8000 abierto"
+# Security Group
+resource "aws_security_group" "nodejs_sg" {
+  name        = "nodejs-security-group"
+  description = "Security group con puerto 8000 abierto para Node.js"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
@@ -22,7 +19,7 @@ resource "aws_security_group" "isc_system_core_sg" {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   egress {
@@ -33,52 +30,56 @@ resource "aws_security_group" "isc_system_core_sg" {
   }
 }
 
-resource "aws_key_pair" "backend-ssh" {
-  key_name   = "isc-system-backend-ssh"
-  public_key = var.ssh_public_key
+# Key Pair
+resource "aws_key_pair" "nodejs-ssh" {
+  key_name   = "nodejs-ssh"
+  public_key = file("id_rsa.pub")
 }
 
-resource "aws_instance" "isc_system_core_server" {
-  ami           = "ami-0e2c8caa4b6378d8c" # Amazon Linux 2 AMI ID
+# EC2 Instance
+resource "aws_instance" "nodejs_server" {
+  ami           = "ami-01816d07b1128cd2d" # Amazon Linux 2
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.backend-ssh.key_name
-  vpc_security_group_ids = [aws_security_group.isc_system_core_sg.id]
+  key_name      = aws_key_pair.nodejs-ssh.key_name
+  vpc_security_group_ids = [aws_security_group.nodejs_sg.id]
 
   tags = {
-    Name = "Backend--ISC-UPB-System-App"
+    Name = "NodeJS-App-Server"
   }
 
-  # Provisión para clonar el repositorio y levantar el backend
+  depends_on = [aws_security_group.nodejs_sg]
+
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
       host        = self.public_ip
       user        = "ec2-user"
-      private_key = file("isc-system-backend-ssh")
+      private_key = file("id_rsa")
     }
 
     inline = [
-      "sudo apt update -y",
-      "sudo apt upgrade -y",
-      "sudo apt install -y nodejs npm git", 
-      "mkdir -p /home/ec2-user/backend",
-      "cd /home/ec2-user",
-      "git clone https://github.com/Khenya/ISC-System-UPB-Core.git backend", 
-      "cd /home/ec2-user/backend",
-      "npm install", # Instalar dependencias del proyecto
-      "echo '#!/bin/bash' > /home/ec2-user/backend/start.sh",
-      "echo 'cd /home/ec2-user/backend' >> /home/ec2-user/backend/start.sh",
-      "echo 'nohup node server.js > nohup.out 2>&1 &' >> /home/ec2-user/backend/start.sh",
-      "chmod +x /home/ec2-user/backend/start.sh",
-      "bash /home/ec2-user/backend/start.sh" # Levantar la aplicación
+      "sudo yum update -y",
+      "sudo yum install -y git",
+      "curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -",
+      "sudo yum install -y nodejs",
+      "git clone https://github.com/Khenya/ISC-System-UPB-Core.git /home/ec2-user/app",
+      "cd /home/ec2-user/app",
+      "npm install",
+      "echo '#!/bin/bash' > /home/ec2-user/start_app.sh",
+      "echo 'cd /home/ec2-user/app' >> /home/ec2-user/start_app.sh",
+      "echo 'nohup npm start > app.log 2>&1 &' >> /home/ec2-user/start_app.sh",
+      "chmod +x /home/ec2-user/start_app.sh",
+      "sudo -u ec2-user bash /home/ec2-user/start_app.sh"
     ]
   }
 }
 
-resource "aws_eip" "backend_eip" {
-  instance = aws_instance.isc_system_core_server.id
+# Elastic IP
+resource "aws_eip" "nodejs_eip" {
+  vpc = true
+}
 
-  tags = {
-    Name = "Backend-ISC-UPB-System-App-EIP"
-  }
+resource "aws_eip_association" "nodejs_eip_association" {
+  instance_id   = aws_instance.nodejs_server.id
+  allocation_id = aws_eip.nodejs_eip.id
 }
